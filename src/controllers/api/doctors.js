77ad0b61765e528e0e758c8327921dcaa05331c7
kailash360 <kailashkejriwal21@gmail.com>
2021-09-19 00:05:22 +0530

    minor fix

--- src/controllers/api/doctors.js
+++ src/controllers/api/doctors.js
@@ -5,8 +5,8 @@ exports.doctors = async(req, res) => {
 
     try {
         let filterObject = {}
-        req.body.filterArray.includes('fees') ? filterObject.fees = 1 : ''
-        req.body.filterArray.includes('experience') ? filterObject.experience = 1 : ''
+        req.body.filterArray.includes('price') ? filterObject.fees = 1 : ''
+        req.body.filterArray.includes('experience') ? filterObject.experience = -1 : ''
         req.body.filterArray.includes('rating') ? filterObject.rating = 1 : ''
 
         console.log(filterObject)
