7a914f7048bc370d8b704a675cdb15c16ffab93d
kailash360 <kailashkejriwal21@gmail.com>
2021-09-18 21:12:17 +0530

    added cors

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express();
 const db = require('./src/config/db.js')
 const auth = require('./src/routes/auth.js')
 const api = require('./src/routes/api.js')
+const cors = require('cors')
+
 app.use(express.json());
+app.use(cors());
 
 app.use('/auth', auth)
 app.use('/api', api)
