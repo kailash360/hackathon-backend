aa8a5ae1b5b70be690ef12aab5f42ab3991c508a
kailash360 <kailashkejriwal21@gmail.com>
2021-09-18 18:35:00 +0530

    added api to confirm apointment request

--- src/controllers/api/appointments.js
+++ src/controllers/api/appointments.js
@@ -10,10 +10,13 @@ exports.requestAppointment = async(req, res) => {
     const date = req.body.date
     const time = req.body.time
 
+    const requestId = patientId + '--' + doctorId + '--' + date
+
     //Find the doctor and add request to the doctor
     await doctors.findOne({ _id: doctorId })
         .then(async(doctor, err) => {
             let newRequest = {
+                requestId: requestId,
                 patientId: patientId,
                 date: date,
                 time: time,
@@ -34,11 +37,12 @@ exports.requestAppointment = async(req, res) => {
         .then(async(patient, err) => {
 
             let newRequest = {
+                requestId: requestId,
                 doctorId: doctorId,
                 date: date,
                 time: time,
                 remarks: remarks,
-                confirmed: false,
+                confirmed: 'Pending',
                 comments: '',
             }
 
@@ -49,9 +53,133 @@ exports.requestAppointment = async(req, res) => {
                     res.send({ 'status': 'error', 'message': err.message })
                 }
 
-                res.send({ 'status': 'success', 'message': 'Appointment created successfully' })
+                res.send({ 'status': 'success', 'message': 'Appointment requested successfully' })
             })
 
         })
+
+}
+
+
+//To confirm/cancel an appointment
+exports.confirmAppointmentRequest = async(req, res) => {
+    const requestId = req.body.requestId
+    const patientId = req.body.patientId
+    const doctorId = req.body.doctorId
+    const confirmed = req.body.confirmed
+    const comments = req.body.comments
+    const date = req.body.date
+
+
+    switch (confirmed) {
+        //If appointment is confirmed
+        case true:
+
+            //save the appointment in doctor
+            let newAppointment = {
+                appointmentId: requestId,
+                patientId: patientId,
+                date: date,
+                remarks: remarks
+            }
+
+            await doctors.findOne({ _id: doctorId })
+                .then(async(doctor, err) => {
+                    if (err) {
+                        res.send({ 'status': 'error', 'message': err.message })
+                    }
+
+                    doctor.futureAppointments.push(newAppointment)
+                    doctor.requests.filter(item => item.requestId !== requestId)
+                    doctor.findOneAndUpdate({ _id: doctorId }, { $set: { futureAppointments: doctor.futureAppointments, requests: doctor.requests } }, { upsert: true }, (result, err) => {
+                        if (err) {
+                            res.send({ 'status': 'error', 'message': err })
+                        }
+                    })
+
+                })
+                //save the appointtment in patient
+            newAppointment = {
+                appointmentId: requestId,
+                doctorId: doctorId,
+                date: date,
+                confirmed: 'true',
+                remarks: remarks,
+                comments: comments
+            }
+            await patients.findOne({ _id: patientId })
+                .then(async(patient, err) => {
+                    if (err) {
+                        res.send({ 'status': 'error', 'message': err.message })
+                    }
+
+                    patient.futureAppointments.push(newAppointment)
+                    patient.requests.map(item => {
+                        if (item.requestId === requestId) {
+                            item.confirmed = true
+                        }
+
+                        return item
+                    })
+                    await patients.findOneAndUpdate({ _id: patientId }, { $set: { futureAppointments: patient.futureAppointments, requests: patient.requests } }, { upsert: true }, (result, err) => {
+                        if (err) {
+                            res.send({ 'status': 'error', 'message': err.message })
+                        }
+
+                        res.send({ 'status': 'success', 'message': 'Appointment confirmed successfully' })
+                    })
+
+                })
+            break
+
+            //If appointment is rejected,
+        case false:
+
+            //Find doctor and remove the request
+            await doctors.findOne({ _id: doctorId })
+                .then(async(doctor, err) => {
+                    if (err) {
+                        res.send({ 'status': 'error', 'message': err.message })
+                    }
+
+                    doctor.requests.filter(request => request.requestId !== requestId)
+                    await doctors.findOneAndUpdate({ _id: doctorId }, { $set: { requests: doctor.requests } }, { upsert: true }, (result, err) => {
+                        if (err) {
+                            res.send({ 'status': 'error', 'message': err.message })
+                        }
+                    })
+                })
+
+            //Find patient and set the request to false
+            await patients.findOne({ _id: patientId })
+                .then(async(patient, err) => {
+                    if (err) {
+                        res.send({ 'status': 'error', 'message': err.message })
+                    }
+
+                    patient.requests.map(item => {
+                        if (item.requestId === requestId) {
+                            item.confirmed = false
+                        }
+
+                        return item
+                    })
+
+                    await patients.findOneAndUpdate({ _id: patientId }, { $set: { requests: patient.requests } }, { upsert: true }, (result, err) => {
+                        if (err) {
+                            res.send({ 'status': 'error', 'message': err.message })
+                        }
+
+                        res.send({ 'status': 'success', 'message': 'Apointment cancelled successfully' })
+                    })
+                })
+            break
+
+        default:
+            res.send({ 'status': 'error', 'message': 'Invalid confirmation status' })
+
+
+
+    }
 
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,13 +2,13 @@
 const express = require("express");
 const router = express.Router();
 const { doctors } = require("../controllers/api/doctors.js")
-const { requestAppointment } = require("../controllers/api/appointments.js")
+const { requestAppointment, confirmAppointmentRequest } = require("../controllers/api/appointments.js")
 
 //All api for doctors
 router.get('/doctors', doctors)
 
 //all apis for appointments
 router.post('/requestAppointment', requestAppointment)
-
+router.post('/confirmAppointmentRequest', confirmAppointmentRequest)
 
 module.exports = router
