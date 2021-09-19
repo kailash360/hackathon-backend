f9d484f476bc1679c647c0f4f8c5e91e8ae91cce
kailash360 <kailashkejriwal21@gmail.com>
2021-09-19 19:34:32 +0530

    minor fix

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,121 +1,101 @@
-const doctors = require('../../models/Doctor.js')
-const patients = require('../../models/Patient.js')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const doctors = require("../../models/Doctor.js");
+const patients = require("../../models/Patient.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 exports.login = async(req, res) => {
-
     try {
-
         //Extract credentials and chek if fields are given
-        const user = req.query.user
+        const user = req.query.user;
         if (!user) {
-            res.send({ 'status': 'error', 'message': 'User type not found' })
+            res.send({ status: "error", message: "User type not found" });
         }
 
-        const email = req.body.email
-        const password = req.body.password
+        const email = req.body.email;
+        const password = req.body.password;
 
         if (!email || !password) {
-            res.send({ 'status': 'error', 'message': 'Fields cannot be empty' })
+            res.send({ status: "error", message: "Fields cannot be empty" });
         }
 
-        switch (user) {
+        if (user === "doctor") {
+            //Search in db and match password
+            await doctors
+                .findOne({ email: email, password: userPassword })
+                .then(async(result, err) => {
+                    if (err) {
+                        res.send({ status: "error", message: err.message });
+                    }
 
-            //If user is a doctor
-            case 'doctor':
+                    if (!result) {
+                        res.send({ status: "error", message: "User not found" });
+                        return;
+                    } else {
+                        const dbPassword = result.password;
 
-                //Search in db and match password
-                await doctors.findOne({ email: email, password: userPassword })
-                    .then(async(result, err) => {
-
-                        if (err) {
-                            res.send({ 'status': 'error', 'message': err.message })
-                        }
-
-                        if (!result) {
-                            res.send({ 'status': 'error', 'message': 'User not found' })
-                            return
-                        } else {
-
-                            const dbPassword = result.password
-
-                            await bcrypt.compare(dbPassword, password, (err, result) => {
-                                if (err) {
-                                    res.send({ 'status': 'error', 'message': err.message })
-                                } else {
-                                    if (!result) {
-                                        res.send({ 'status': 'error', 'message': 'Incorrect password' })
-                                        return
-                                    }
+                        await bcrypt.compare(dbPassword, password, (err, result) => {
+                            if (err) {
+                                res.send({ status: "error", message: err.message });
+                            } else {
+                                if (!result) {
+                                    res.send({ status: "error", message: "Incorrect password" });
+                                    return;
                                 }
-                            })
-
-                            //If password matches, send proper data
-                            console.log(result)
-                            const user = {
-                                name: result.name,
-                                email: result.email,
-                                userId: result._id,
-                                type: 'doctor'
                             }
+                        });
 
-                            const token = jwt.sign(user, process.env.JWT_KEY)
-                            res.send({ 'status': 'success', 'user': user, 'token': token })
-                            return
-                        }
-                    })
+                        //If password matches, send proper data
+                        console.log(result);
+                        const user = {
+                            name: result.name,
+                            email: result.email,
+                            userId: result._id,
+                            type: "doctor",
+                        };
 
-                break
+                        const token = jwt.sign(user, process.env.JWT_KEY);
+                        res.send({ status: "success", user: user, token: token });
+                        return;
+                    }
+                });
+        } else {
+            await patients.findOne({ email: email }).then(async(result, err) => {
+                if (err) {
+                    res.send({ status: "error", message: err.message });
+                }
 
-            case 'patient':
+                if (!result) {
+                    res.send({ status: "error", message: "User not found" });
+                } else {
+                    const dbPassword = result.password;
 
-                await patients.findOne({ email: email })
-                    .then(async(result, err) => {
-
+                    await bcrypt.compare(dbPassword, password, (err, result) => {
                         if (err) {
-                            res.send({ 'status': 'error', 'message': err.message })
-                        }
-
-                        if (!result) {
-                            res.send({ 'status': 'error', 'message': 'User not found' })
+                            res.send({ status: "error", message: err.message });
                         } else {
-
-                            const dbPassword = result.password
-
-                            await bcrypt.compare(dbPassword, password, (err, result) => {
-                                if (err) {
-                                    res.send({ 'status': 'error', 'message': err.message })
-                                } else {
-                                    if (!result) {
-                                        res.send({ 'status': 'error', 'message': 'Incorrect password' })
-                                    }
-                                }
-                            })
-
-                            console.log(result)
-                            const user = {
-                                name: result.name,
-                                email: result.email,
-                                userId: result._id,
-                                type: 'patient',
-                                age: result.age,
-                                contact: result.contact
+                            if (!result) {
+                                res.send({ status: "error", message: "Incorrect password" });
                             }
-
-                            const token = jwt.sign(user, process.env.JWT_KEY)
-                            res.send({ 'status': 'success', 'user': user, 'token': token })
                         }
-                    })
+                    });
 
-                break
-            default:
-                res.send({ 'status': 'error', 'message': 'Invalid user' })
-                break
+                    console.log(result);
+                    const user = {
+                        name: result.name,
+                        email: result.email,
+                        userId: result._id,
+                        type: "patient",
+                        age: result.age,
+                        contact: result.contact,
+                    };
+
+                    const token = jwt.sign(user, process.env.JWT_KEY);
+                    res.send({ status: "success", user: user, token: token });
+                }
+            });
         }
     } catch (err) {
-        console.error(err)
-        res.send({ 'status': 'error', 'message': err.message })
+        console.error(err);
+        res.send({ status: "error", message: err.message });
     }
-
-}
+};
--- src/controllers/auth/signup.js
+++ src/controllers/auth/signup.js
@@ -1,92 +1,86 @@
-const patients = require('../../models/Patient.js')
-const doctors = require('../../models/Doctor.js')
-const { hashPassword } = require('../../utils/password.js')
+const patients = require("../../models/Patient.js");
+const doctors = require("../../models/Doctor.js");
+const { hashPassword } = require("../../utils/password.js");
 
 exports.signup = async(req, res) => {
-
     try {
-
         //check if user type is provided or not
-        const user = req.query.user
+        const user = req.query.user;
         if (!user) {
-            return res.send({ 'status': 'error', 'message': 'User type not provided' })
+            return res.send({ status: "error", message: "User type not provided" });
         }
 
-        const name = req.body.name
-        const email = req.body.email
-        const contact = req.body.contact
-        const password = req.body.password
+        const name = req.body.name;
+        const email = req.body.email;
+        const contact = req.body.contact;
+        const password = req.body.password;
 
-        switch (user) {
-
+        if (user === "doctor") {
             //When user is a doctor
-            case 'doctor':
-                //body of a doctor should contain {name,email,contact,password,,experience,fees,specialization}
-                const experience = parseInt(req.body.experience)
-                const fees = parseInt(req.body.fees)
-                const specialization = req.body.specialization
 
-                let doctor = new doctors()
-                doctor.name = name
-                doctor.email = email
-                doctor.contact = contact
-                await hashPassword(password).then(result => {
-                    doctor.password = result
-                })
-                doctor.experience = experience
-                doctor.fees = fees
-                doctor.specialization = specialization
-                doctor.rating = 0
-                doctor.totalRatings = 0
-                doctor.pastAppointments = []
-                doctor.futureAppointments = []
-                doctor.nextAppointment = {}
+            const experience = parseInt(req.body.experience);
+            const fees = parseInt(req.body.fees);
+            const specialization = req.body.specialization;
 
-                await doctor.save((err, result) => {
-                    if (err) {
-                        console.error(err)
-                        res.send({ 'status': 'error', 'message': err.message })
-                    } else {
-                        res.send({ 'status': 'success', 'message': 'doctor created successfully' })
-                    }
-                })
-                break
+            let doctor = new doctors();
+            doctor.name = name;
+            doctor.email = email;
+            doctor.contact = contact;
+            await hashPassword(password).then((result) => {
+                doctor.password = result;
+            });
+            doctor.experience = experience;
+            doctor.fees = fees;
+            doctor.specialization = specialization;
+            doctor.rating = 0;
+            doctor.totalRatings = 0;
+            doctor.pastAppointments = [];
+            doctor.futureAppointments = [];
+            doctor.nextAppointment = {};
 
-                //When user is a patient
-            case 'patient':
+            await doctor.save((err) => {
+                if (err) {
+                    console.error(err);
+                    res.send({ status: "error", message: err.message });
+                } else {
+                    res.send({
+                        status: "success",
+                        message: "doctor created successfully",
+                    });
+                }
+            });
+        }
+        //When user is a patient
+        else {
+            //body of a patient should contain {name,email,age,contact,password}
+            const age = parseInt(req.body.age);
 
-                //body of a patient should contain {name,email,age,contact,password}
-                const age = parseInt(req.body.age)
+            let patient = new patients();
+            patient.name = name;
+            patient.email = email;
+            patient.contact = contact;
+            patient.age = age;
+            await hashPassword(password).then((result) => {
+                patient.password = result;
+            });
+            patient.pastAppointments = [];
+            patient.futureAppointments = [];
+            patient.nextAppointment = {};
 
-                let patient = new patients()
-                patient.name = name
-                patient.email = email
-                patient.contact = contact
-                patient.age = age
-                await hashPassword(password).then(result => {
-                    patient.password = result
-                })
-                patient.pastAppointments = []
-                patient.futureAppointments = []
-                patient.nextAppointment = {}
-
-                await patient.save((err, result) => {
-                    if (err) {
-                        console.error(err)
-                        res.send({ 'status': 'error', 'message': err.message })
-                    } else {
-                        res.send({ 'status': 'success', 'message': 'patient created successfully' })
-                    }
-                })
-                break
-
-            default:
-                res.send({ 'status': 'error', 'message': 'Invalid user type' })
-
+            await patient.save((err) => {
+                if (err) {
+                    console.error(err);
+                    res.send({ status: "error", message: err.message });
+                } else {
+                    res.send({
+                        status: "success",
+                        message: "patient created successfully",
+                    });
+                }
+            });
         }
     } catch (e) {
-        console.error(e)
-        res.send({ 'status': 'error', 'message': err })
+        console.error(e);
+        res.send({ status: "error", message: err });
     }
-
-}
+};
