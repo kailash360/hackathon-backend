14a9fb6f733183398fa04db97bccf5df0e67ed9b
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-09-20 18:24:16 +0530

    FIxed login and signup route

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -3,99 +3,53 @@ const patients = require("../../models/Patient.js");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-exports.login = async(req, res) => {
-    try {
-        //Extract credentials and chek if fields are given
-        const user = req.query.user;
-        if (!user) {
-            res.send({ status: "error", message: "User type not found" });
-        }
-
-        const email = req.body.email;
-        const password = req.body.password;
-
-        if (!email || !password) {
-            res.send({ status: "error", message: "Fields cannot be empty" });
-        }
-
-        if (user === "doctor") {
-            //Search in db and match password
-            await doctors
-                .findOne({ email: email, password: userPassword })
-                .then(async(result, err) => {
-                    if (err) {
-                        res.send({ status: "error", message: err.message });
-                    }
-
-                    if (!result) {
-                        res.send({ status: "error", message: "User not found" });
-                        return;
-                    } else {
-                        const dbPassword = result.password;
-
-                        await bcrypt.compare(dbPassword, password, (err, result) => {
-                            if (err) {
-                                res.send({ status: "error", message: err.message });
-                            } else {
-                                if (!result) {
-                                    res.send({ status: "error", message: "Incorrect password" });
-                                    return;
-                                }
-                            }
-                        });
-
-                        //If password matches, send proper data
-                        console.log(result);
-                        const user = {
-                            name: result.name,
-                            email: result.email,
-                            userId: result._id,
-                            type: "doctor",
-                        };
-
-                        const token = jwt.sign(user, process.env.JWT_KEY);
-                        res.send({ status: "success", user: user, token: token });
-                        return;
-                    }
-                });
-        } else {
-            await patients.findOne({ email: email }).then(async(result, err) => {
-                if (err) {
-                    res.send({ status: "error", message: err.message });
-                }
-
-                if (!result) {
-                    res.send({ status: "error", message: "User not found" });
-                } else {
-                    const dbPassword = result.password;
-
-                    await bcrypt.compare(dbPassword, password, (err, result) => {
-                        if (err) {
-                            res.send({ status: "error", message: err.message });
-                        } else {
-                            if (!result) {
-                                res.send({ status: "error", message: "Incorrect password" });
-                            }
-                        }
-                    });
-
-                    console.log(result);
-                    const user = {
-                        name: result.name,
-                        email: result.email,
-                        userId: result._id,
-                        type: "patient",
-                        age: result.age,
-                        contact: result.contact,
-                    };
-
-                    const token = jwt.sign(user, process.env.JWT_KEY);
-                    res.send({ status: "success", user: user, token: token });
-                }
-            });
-        }
-    } catch (err) {
-        console.error(err);
-        res.send({ status: "error", message: err.message });
+exports.login = async (req, res) => {
+  try {
+    //Extract credentials and chek if fields are given
+    const user = req.query.user;
+    if (!user) {
+      res.send({ status: "error", message: "User type not found" });
     }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let dbuser, dbPassword;
+
+    if (user === "doctor") {
+      dbuser = await doctors.findOne({ email: email });
+      dbPassword = dbuser.password;
+    } else {
+      dbuser = await patients.findOne({ email: email });
+      dbPassword = dbuser.password;
+    }
+
+    console.log(dbuser, dbPassword);
+
+    dbuser.comparePassword(password, (err, result) => {
+      if (err) {
+        res.status(500).json({
+          message: "Internal Server Error",
+        });
+      } else {
+        if (!result) {
+          res.send({ status: "error", message: "Incorrect password" });
+        } else {
+          const resuser = {
+            name: dbuser.name,
+            email: dbuser.email,
+            userId: dbuser._id,
+            type: user,
+          };
+
+          const token = jwt.sign(resuser, process.env.JWT_KEY);
+          res.send({ status: "success", user: resuser, token: token });
+          return;
+        }
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.send({ status: "error", message: err.message });
+  }
 };
